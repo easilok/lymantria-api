f5cf6acc0399b0fb0f789bee3070b5c580e3327a
Luis Pereira <luis.pereira@addvolt.com>
2023-03-12 23:42:37 +0000

    added docker-compose.dev for database development setup, and seed for types
    Types are not automatically created by gorm on postgres database

---
+++ database/seed/1.create_types.sql
@@ -0,0 +1,10 @@
+CREATE TYPE public.enum_butterfly_rarity AS ENUM (
+    'common',
+    'rare',
+    'ultrarare'
+);
+
+CREATE TYPE public.enum_butterfly_daytime AS ENUM (
+    'day',
+    'night'
+);
